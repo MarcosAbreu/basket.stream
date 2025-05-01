412692efc982c85b484448403777bff5df9cd9fc
Marcos Abreu <marcosabreudasilva@gmail.com>
2025-05-01 20:03:54 -0300

    feat(config):  added remote images configuration for nextjs

--- basket-stream/next.config.ts
+++ basket-stream/next.config.ts
@@ -1,9 +1,29 @@
-/** @type {import('next').NextConfig} */
+import { NextConfig } from "next";
 
-const nextConfig = {
+const API_URL = `${process.env.NEXT_PUBLIC_BASKET_STREAM_API}`;
+
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_BASKET_STREAM_API is not defined in .env.local file");
+}
+
+const url = new URL(API_URL);
+
+// Validate protocol
+if (url.protocol !== "http:" && url.protocol !== "https:") {
+  throw new Error(`Unsupported protocol: ${url.protocol}`);
+}
+
+const nextConfig: NextConfig = {
   reactStrictMode: false,
-  experimental: {
-    appDir: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: url.protocol.slice(0, -1) as "http" | "https",
+        hostname: url.hostname,
+        port: url.port,
+        pathname: "/**",
+      },
+    ],
   },
 };
 
